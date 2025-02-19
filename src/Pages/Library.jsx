f5f8a4615680bedbf261e3e87109f9f913836e2e
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-02-20 00:06:52 +0100

    new

--- src/Pages/Library.jsx
+++ src/Pages/Library.jsx
@@ -4,15 +4,14 @@ import Resources from "../Components/Resources"
 function Library() {
   return (
     <>
-      <div className="relative h-96 lg:h-screen">
+      <div className="relative h-80 md:h-[35rem] lg:h-[30rem]">
         <div className="absolute bg-no-repeat inset-0 bg-[url('/seren.webp')] bg-cover bg-bottom before:absolute before:inset-0 before:bg-gradient-to-b before:from-black/50 before:to-black/55">
-          <div className="relative p-5 mx-auto my-20 md:my-15 lg:my-40  ">
+          <div className="relative p-5 mx-auto my-20 md:my-56 lg:my-48  ">
             <div className="flex-col justify-center items-center flex gap-8 place-items-center text-white">
               <h4 className=" uppercase text-center text-4xl lg:text-7xl font-bold">
                 our resources
               </h4>
-              <p className="text-center text-md lg:text-xl">In a world where mental health challenges are escalating, leaving individuals vulnerable to stress,<br className="hidden sm:block" /> job loss and more, the need for effective therapy and counseling has reached an overwhelming proportion
-              </p>
+              
             </div>
           </div>
         </div>
