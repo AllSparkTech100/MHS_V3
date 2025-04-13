6f774b83575b324c8d3897153438a195f573b585
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-04-12 16:24:52 -1100

    latest

--- src/Components/Forms/FormData.jsx
+++ src/Components/Forms/FormData.jsx
@@ -1,5 +1,5 @@
 import { LuSend } from "react-icons/lu";
-import {useState} from 'react';
+import { useState } from 'react';
 import { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
@@ -10,24 +10,24 @@ function FormData() {
         email: "",
         number: "",
         message: "",
-      });
+    });
 
     const form = useRef();
 
-    const formChange = (e) =>{
+    const formChange = (e) => {
         setFormSet({ ...formSet, [e.target.name]: e.target.value });
     }
     const handleSubmit = (e) => {
         e.preventDefault();
 
         emailjs
-            .sendForm( import.meta.env.VITE_REACT_APP_EMAIL_JS_SERVICE_ID, import.meta.env.VITE_REACT_APP_EMAIL_JS_TEMPLATE_ID, form.current, 
+            .sendForm(import.meta.env.VITE_REACT_APP_EMAIL_JS_SERVICE_ID, import.meta.env.VITE_REACT_APP_EMAIL_JS_TEMPLATE_ID, form.current,
                 import.meta.env.VITE_REACT_APP_EMAIL_JS_PUBLIC_KEY,
             )
             .then(
                 () => {
-                   alert('SUCCESS!');
-                   setFormSet({ name: "", email: "", number: "", message: "" });
+                    alert('SUCCESS!');
+                    setFormSet({ name: "", email: "", number: "", message: "" });
                 },
                 (error) => {
                     alert('FAILED...', error.text);
@@ -37,30 +37,30 @@ function FormData() {
 
     return (
         <>
-        
+
             <form ref={form} onSubmit={handleSubmit} >
                 {/* FullName */}
                 <div className="mb-3 flex gap-3 flex-col mt-3">
                     <label className="font-semibold" htmlFor="name">Full Name</label>
                     <input type="text" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="name"
-                     value={formSet.name}
-                     onChange={formChange} id="" placeholder="e.g James Richard" required />
+                        value={formSet.name}
+                        onChange={formChange} id="" placeholder="e.g James Richard" required />
 
                 </div>
                 {/* Email Address */}
                 <div className="mb-3 cols-span-1 flex gap-3 flex-col mt-3">
                     <label className="font-semibold" htmlFor="email">Email </label>
                     <input type="email" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="email"
-                         value={formSet.email}
-                         onChange={formChange}
+                        value={formSet.email}
+                        onChange={formChange}
                         id="" placeholder="e.g jamesrichard@hotmail.com" required />
                 </div>
                 {/* Mobile Number */}
                 <div className="mb-3 flex gap-3 flex-col mt-3">
                     <label className="font-semibold" htmlFor="number">Mobile Number </label>
                     <input type="phone" className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="number"
-                         value={formSet.number}
-                         onChange={formChange}
+                        value={formSet.number}
+                        onChange={formChange}
                         id="" placeholder="e.g +234123456788" required />
                 </div>
                 {/* Category */}
@@ -69,8 +69,8 @@ function FormData() {
                 <div className="mb-3 flex gap-3 flex-col mt-3">
                     <label className="font-semibold" htmlFor="message">Message </label>
                     <textarea className="p-3 placeholder:text-black/40 outline border-gray-700 outline-1 bg-white focus:outline-orange-500 w-full rounded-md" name="message"
-                         value={formSet.message}
-                         onChange={formChange}
+                        value={formSet.message}
+                        onChange={formChange}
 
                         id="" cols="30" rows="10" ></textarea>
                 </div>
