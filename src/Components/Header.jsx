1011ffd9431203bd063f3ba55250683128575021
AllSparkTech100 <ikangenyinabasiono@gmail.com>
2025-06-09 19:29:45 +0100

    updated user experience

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { IoClose, IoMenuSharp } from "react-icons/io5";
+import { motion, AnimatePresence } from "framer-motion";
 
 
 
@@ -22,7 +23,7 @@ function Header() {
   }, [isMenu])
   return (
     <>
-      <header className="sticky top-0 z-10 bg-white text-white">
+      <header className="sticky top-0 z-[9999] bg-white text-white">
         <div className="flex gap-5 justify-between h-20 items-center p-4 lg:p-2">
 
           <img href='/' src="/mhslogo.webp" alt="Logo" className="w-15 h-10 lg:ml-4" />
@@ -47,39 +48,52 @@ function Header() {
             Book a Session
           </button>
           <div className="flex items-center flex-row justify-center">
-            <button
+
+            <motion.button
               onClick={menuToggle}
               className="text-2xl text-black md:block lg:hidden sm:hidden focus:outline-none"
+              animate={{ rotate: isMenu ? 90 : 0, scale: isMenu ? 1.2 : 1 }}
+              transition={{ type: "spring", stiffness: 300, damping: 20 }}
             >
               {isMenu ? <IoClose size={28} /> : <IoMenuSharp size={28} />}
-            </button>
+            </motion.button>
           </div>
-        </div>{" "}
-        {isMenu && (
-          <div className="sm:hidden md:block lg:hidden min-h-screen fixed w-full text-white fill-transparent bg-emerald-950 transition-colors ease-linear duration-100">
-            <div className="bg-slate-800 absolute z-10 transition-colors ease-linear duration-300 min-h-screen w-80 pt-5">
-              <div className=" text-white p-5">
-                <nav
-                  className="uppercase  text-xl">
-                  {" "}
-                  <ul className="text-left flex flex-col items-start gap-5">
-                    <li className=""> <a href="/">Home</a></li>
-                    <li className=""><a href="/about">About Us</a></li>
-                    <li className=""><a href="/contact">Contact Us</a></li>
-                    <li className=""><a href="/services">Our Services</a></li>
-                    <li className=""> <a href="/library"> Library</a></li>
-                  </ul>
-                  <button className="mt-3 text-left text-black bg-white rounded-lg w-full p-3 place-items-center">
-                    <a href="http://">Book a Session</a>
-                  </button>
-                </nav>
-              </div>
-            </div>
-          </div>
-        )}
-
+        </div>
+        <AnimatePresence>
+          {isMenu && (
+            <motion.div initial={{ x: "-100%" }}
+              animate={{ x: 0 }}
+              exit={{ x: "-100%" }}
+              transition={{ type: "spring", stiffness: 300, damping: 30, delay: 0.5 }}
+              className="sm:hidden md:block lg:hidden min-h-screen fixed w-full text-white fill-transparent bg-emerald-950/85 transition-colors ease-linear duration-100"
+              onClick={menuToggle}>
+              <motion.div initial={{ x: -100, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                exit={{ x: -100, opacity: 0 }}
+                transition={{ type: "spring", stiffness: 300, damping: 30, delay: 1 }}
+                className="bg-slate-800 absolute z-10 transition-colors ease-linear duration-300 min-h-screen md:w-1/2 w-80 pt-5"
+                onClick={e => e.stopPropagation()}>
+                <div className=" text-white p-5">
+                  <nav
+                    className="uppercase text-xl">
+                    {" "}
+                    <ul className="text-left flex flex-col items-start gap-5">
+                      <li className="md:mt-7"> <a href="/">Home</a></li>
+                      <li className="md:mt-7"><a href="/about">About Us</a></li>
+                      <li className="md:mt-7"><a href="/contact">Contact Us</a></li>
+                      <li className="md:mt-7"><a href="/services">Our Services</a></li>
+                      <li className="md:mt-7"> <a href="/library"> Library</a></li>
+                    </ul>
+                    <button className="mt-3 md:mt-12 text-center text-black bg-white rounded-lg w-full p-3 place-items-center">
+                      <a href="http://">Book a Session</a>
+                    </button>
+                  </nav>
+                </div>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </header>
-
     </>
   );
 }
